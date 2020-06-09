ebb86900ede7e723159ba4f2688c5072aa13f1b6
Simon Chénard <chenard.simon@gmail.com>
2020-06-09 12:19:52 -0400

    bugfix when calling dataset service, missing auth header and proper path

--- candig_metadata/metadata/backend.py
+++ candig_metadata/metadata/backend.py
@@ -5,17 +5,22 @@ import requests
 
 
 def create_or_query_dataset(dataset_id):
-    url = settings.DATASET_SERVICE_URL
+    url = f"{settings.DATASET_SERVICE_URL}/v2/datasets"
 
     if not url:
         raise Exception('Dataset service is not configured properly')
 
+    # TODO: Actually implement authorization ASAP please
+    headers = {
+        'Authorization': 'not enforced currently'
+    }
+
     data = {
-        "id": uuid4(),
+        "id": str(uuid4()),
         "name": dataset_id
     }
 
-    res = requests.post(url, data=data)
+    res = requests.post(url, headers=headers, json=data)
 
     # TODO: hmm I might not have used 405 method not allowed
     # in that context...
